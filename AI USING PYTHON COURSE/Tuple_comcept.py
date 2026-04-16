85a043a4e8e4902b90bb787511080746186d2684
Meer Hassan Shah <hassanshahvlogs1@gmail.com>
2026-04-17 02:11:12 +0500

    List comcept

---
+++ AI USING PYTHON COURSE/Tuple_comcept.py
@@ -0,0 +1,23 @@
+#Tuple concept
+
+color = ("blue","green","yellow")
+print(color)
+
+# Access tuple
+print(color[1]) 
+
+# Tuple properites 
+# tupe are immutable (cannot be change)
+# color[2]= "orange"                 !this will be cause of error
+
+
+#tuple methods  
+number=(1, 2, 3, 4, 2, 6, 2, 2)
+print(number.count(2))           #count the elements 
+print(number.index(6))           #show the vlaue index
+
+
+
+#set
+my_set={0 ,1 ,2 ,3 ,3 ,4 ,5 ,6 ,1 ,3 ,4 ,2 ,10} # not allowed duplicate
+print(my_set)
---
+++ AI USING PYTHON COURSE/set_concept.py
@@ -0,0 +1,18 @@
+#set
+my_set={0 ,1 ,2 ,3 ,3 ,4 ,5 ,6 ,1 ,3 ,4 ,2 }     # not allowed duplicate
+print(my_set)
+
+#set method 
+my_set.add(10)     #add item
+my_set.remove(6)   #remove item
+print(my_set) 
+
+
+#set operations 
+A={1,2,3,4}
+B={4,5,6,7}
+
+print(A.union(B))
+print(A.intersection(B))
+print(A.difference(B))
+
