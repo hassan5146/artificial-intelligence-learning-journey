35f6c52c9639dad56d7dbc22c122e6c52391235e
Meer Hassan Shah <hassanshahvlogs1@gmail.com>
2026-04-18 01:03:45 +0500

    AI USING PYTHON CONCEPT

---
+++ AI USING PYTHON COURSE/def-calling-function.py
@@ -0,0 +1,16 @@
+#simple function calling
+
+def func():
+    print("HELLO WORLD " )
+
+func()
+    
+
+#simple function calling but change the some world
+def func1(text):
+    print("WELCOME TO MY" , text)
+
+func1("HOME")
+func1("SCHOOL")
+
+    
---
+++ AI USING PYTHON COURSE/return value of function.py
@@ -0,0 +1,14 @@
+#for addition
+def add(a,b):
+    return a + b
+result = add(5,7)
+print(result)
+
+
+
+#for square 
+def square(num):
+    return num * num
+
+result = square(5)
+print(result)
